645d6f7696fa67fe5909a16e4a05560d20b6ec27
David Warshawsky <david.warshawsky@sjsu.edu>
2020-08-12 16:45:45 -0700

    I added a file checker to the generate output function to make sure that a ValueError is raised if the file already exists. This means that the router already has a saved configuration. I will figure out a way to in the main module ask for a new router hostname or to overwrite the current output.

--- router.py
+++ router.py
@@ -1,93 +1,55 @@
+import weakref
 from ipaddress import IPv4Network, IPv4Address
-
+from interfaces import Interface
+import os
 class Router:
     hostname = None
-
-    networks = dict()
-    network_addresses = []
-
-    rip_commands = ["ip routing","router rip","version 2","no auto-summary"]
     commands = ["en","conf t","no ip domain-lookup"]
 
-    def __init__(self):
-        self.hostname = input("Please enter hostname: ")
+    def __init__(self,hostname):
+        # If the hostname already exists, then raise a ValueError
+        if sum([hostname == router.hostname for router in self.getinstances()]) != 0:
+            raise ValueError(f"Router hostname <{hostname}> already exists")
+        self.hostname = hostname
         self.commands += ["hostname %s" % self.hostname]
 
-    def add_interface(self):
+    def add_interface(self,interface:Interface):
+        self.interfaces.append(interface)
 
-        interface  = input("Ex: g2/2 fa2/5 | Please enter interface: ")
-        net_id = input("Ex: 192.168.0.0/25 | Please enter network id for %s with a subnet: " % interface)
-        self.network_addresses.append(net_id.split("/")[0])
-        network  = IPv4Network(net_id)
+    # Used to check that two routers with the same hostname do not exist.
+    @classmethod
+    def getinstances(cls):
+        dead = set()
+        for ref in cls._instances:
+            obj = ref()
+            if obj is not None:
+                yield obj
+            else:
+                dead.add(ref)
+        cls._instances -= dead
 
-        address = input("Ex: 192.168.1.2 | Please enter IP Address for int %s: " % interface)
-        mask = str(network.netmask)
-        self.networks[interface] = [address,mask]
-
-    def get_net_addrs(self):
-        return self.network_addresses
-
-    def get_ip_addresses(self):
-        ip_addresses = []
-        for value in self.networks:
-            ip_addresses.append(value[0])
-        return ip_addresses
-
-    def configure_rip(self):
-        for addr in self.get_net_addrs():
-            self.rip_commands.append("network %s" % addr)
-        self.rip_commands.append("exit")
-        self.commands += self.rip_commands
+    def configure_interfaces(self):
+        for interface,values in self.networks.items():
 
 
     def done(self):
         # ["end", "wr mem"]
-        for _int,values in self.networks.items():
-            # Enter interface
-            self.commands += ["int %s" % _int]
-            # Make sure you can change it
-            self.commands += ["no switchport"]
-            # Set the ip address of the interface
-            self.commands += ["ip address %s %s" % (values[0],values[1])]
-            # Stop the port interface from going down
-            self.commands += ["no shut"]
-            # Get out of the interface
-            self.commands  += ["exit"]
+
         # Configure rip
-        self.configure_rip()
         self.commands += ["end","wr mem"]
         self.generate_output()
 
 
-
-
     def generate_output(self):
-        output_file = open("%s.txt" %self.hostname, "w")
+        filename = "%s.txt" %self.hostname
+        if (os.path.isfile(filename)):
+            raise ValueError(f"There is already a file called {filename}")
+        output_file = open(filename, "w")
         for command in self.commands:
             output_file.write(command + "\n ! \n")
         output_file.close()
 
 
-
-def main1():
-    prompt = " N for new router \n A for a new interface \n D for done with current router \n Q to finish\n R for reminder\n:"
-    inp = input(prompt)
-    while inp != "Q":
-        if (inp == "N"):
-            router = Router()
-            print("You created a router called %s" % router.hostname)
-        if (inp == "A"):
-            router.add_interface()
-            print("You created an interface")
-        if (inp == "D"):
-            print("Check this directory for a text file with this hostname you provided")
-            router.done()
-        if (inp == "R"):
-            print(prompt)
-        inp = input("R for a reminder of commands | Enter command: ")
-
-if __name__ == '__main__':
-    main1()
 
 
 
