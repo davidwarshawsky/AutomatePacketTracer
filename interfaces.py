bffeb747056070b9605a6614c0ed0433e8c9129d
David Warshawsky <david.warshawsky@sjsu.edu>
2020-08-12 22:55:42 -0700

    I found a function that takes a CIDR and turns it into a network id and network mask. I then implemented it within the setter of the CIDR and now I need to add a test for it.

--- interfaces.py
+++ interfaces.py
@@ -2,6 +2,8 @@ interface  = input("Ex: g2/2 fa2/5 | Please enter interface: ")
 net_id = input("Ex: 192.168.0.0/25 | Please enter network id for %s with a subnet: " % interface)
 address = input("Ex: 192.168.1.2 | Please enter IP Address for int %s: " % interface)
 import re
+import socket
+import struct
 class Interface(object):
     """
     Interface works with fast ethernet and gigabit ethernet ports.
@@ -11,12 +13,12 @@ class Interface(object):
     # Example: fa 0/1 | g 0/2
     port: [str]     = None
     # Example: 192.168.1.0/24
-    cidr: str       = None
+    cidr: [str]     = None
     # The actual ip address assigned to the interface.
     # Example: 192.168.1.1
     ip_address: str = None
 
-    def __init__(self,port:str,cidr:str,ip_address):
+    def __init__(self,port:str,cidr:str,ip_address:str):
         self.port = port
         self.cidr = cidr
         self.ip_address = ip_address
@@ -43,20 +45,33 @@ class Interface(object):
         return port_split
 
     @property
-    def cidr(self):
+    def cidr(self) -> [str]:
         return self.cidr
 
     @cidr.setter
-    def cidr(self,cidr):
+    def cidr(self,cidr) -> [str]:
         cidr_split = re.split("/",cidr)
         if len(cidr_split) != 2:
             raise ValueError(f'<{cidr}> does not contain "/"')
         valid_subnet_address = '\d{,3}.\d{,3}.\d{,3}.\d{,3}'
         if not bool(re.match(valid_subnet_address,cidr_split[0])):
             raise ValueError(f'<{cidr_split[0]}> is not a valid subnet address')
-        valid_subnet_mask   = '^\d$|^[1-3][0-2]$'
-        if
-        return cidr_split;
+        cidr_netmask = int(cidr_split[1])
+        if cidr_netmask < 0 or cidr_netmask > 32:
+            raise ValueError(f"<{cidr_split[1]}> is not a valid subnet mask")
+        network,netmask = self.cidr_to_netmask(cidr)
+        self.cidr = [network,netmask]
+
+    @staticmethod
+    # https://stackoverflow.com/questions/33750233/convert-cidr-to-subnet-mask-in-python
+    # "<<" and ">>" are bitwise operators that move the binary value over by the number of digits.
+    def cidr_to_netmask(cidr):
+        network, net_bits = cidr.split('/')
+        host_bits = 32 - int(net_bits)
+        # ntoa only supports 32 bit IPV4, use ntop to support ipv6
+        # todo support IPV6
+        netmask = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
+        return network, netmask
 
     def initial_setup(self):
         # Enter the interface.
