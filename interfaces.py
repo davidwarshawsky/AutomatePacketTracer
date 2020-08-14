17ba08a2209d846a97891eb91067a58ae3b94dd7
David Warshawsky <david.warshawsky@sjsu.edu>
2020-08-14 15:35:00 -0700

    I changed all attributes that need to be set into properties. The attributes are accessed as strings while the internal attribute is a list of strings that starts with an "_".Example: port: 'fa 0/1' | _port: ['fa','0/1'] cidr: '192.168.0.0/24' | _cidr = ['192.168.0.0','24'] ip_address: '192.168.1.1 | _ip_address: ['192','168','1','1']
    The init commands are stored in an array to enter enter the interface
    while the configuration command will set the interface's IP address
    while the exit commands bring the program back to basic configuration
    mode.

--- interfaces.py
+++ interfaces.py
@@ -1,6 +1,6 @@
-interface  = input("Ex: g2/2 fa2/5 | Please enter interface: ")
-net_id = input("Ex: 192.168.0.0/25 | Please enter network id for %s with a subnet: " % interface)
-address = input("Ex: 192.168.1.2 | Please enter IP Address for int %s: " % interface)
+# interface  = input("Ex: g2/2 fa2/5 | Please enter interface: ")
+# net_id = input("Ex: 192.168.0.0/25 | Please enter network id for %s with a subnet: " % interface)
+# address = input("Ex: 192.168.1.2 | Please enter IP Address for int %s: " % interface)
 import re
 import socket
 import struct
@@ -9,58 +9,108 @@ class Interface(object):
     Interface works with fast ethernet and gigabit ethernet ports.
     TO DO: Provide support for serial interfaces.
     """
+    # Make sure you can change the interface.
+    init_commands: [str]   = [None,"no switchport"]
+    config_commands: [str] = [None]
+    # Stop the port interface from going down
+    # Get out of the interface
+    exit_commands = ["no shut","exit"]
     commands: [str] = []
     # Example: fa 0/1 | g 0/2
-    port: [str]     = None
+    _port: [str]     = None
+    port: str        = None
     # Example: 192.168.1.0/24
-    cidr: [str]     = None
+    # Consists of network,prefix
+    _cidr: [str]     = None
+    cidr: str        = None
     # The actual ip address assigned to the interface.
-    # Example: 192.168.1.1
-    ip_address: str = None
+    # Example: [192,168,1,1]
+    _ip_address: [str] = None
+    ip_address:  str   = None
+    # Example 255.255.255.0
+    netmask: str       = None
 
     def __init__(self,port:str,cidr:str,ip_address:str):
         self.port = port
         self.cidr = cidr
         self.ip_address = ip_address
-        self.initial_setup()
-        self.configure_setup()
-        self.exit_setup()
+        self.init_setup()
+        self.config_setup()
 
     @property
-    def port(self):
+    def commands(self) -> [str]:
+        commands = self.init_commands + self.config_commands + self.exit_commands
+        return commands
+
+    @property
+    def port(self) -> str:
         # A list of valid regex for the first part of the port.
-        return self.port
+        return ' '.join(self._port)
+
+    @property
+    def cidr(self) -> str:
+        return '/'.join(self._cidr)
+
+    @property
+    def ip_address(self) -> str:
+        return '.'.join(self._ip_address)
 
     @port.setter
-    def port(self,port):
+    def port(self,port:str):
+        # todo: Add support for VLAN(You'll support Switches Later).
         valid_port_types = ['^fastethernet$', '$f$', '^fa$', '^gigabitethernet$', '^g$', '^gb$']
         # Split the port by spaces.
         port_split = port.split()
+        # If the port is not split by spaces, then split it by letters and numbers separated by "/".
+        if len(port_split) != 2:
+            port_split = re.match("([a-zA-Z]+)([0-9]+/[0-9]+)",port_split[0])
+            if port_split == None:
+                raise ValueError(f"<{port}> is not a valid port name")
+            port_split = list(port_split.groups())
         # If the first part of the port is not a valid port type, raise a ValueError.
         if sum([bool(re.match(port_name, port_split[0])) for port_name in valid_port_types]) != 1:
             raise ValueError(f'<{port_split[0]}> is not a valid port name')
         # If the second part of the port does not contain '/', raise a ValueError.
         if '/' not in port_split[1]:
-            raise ValueError(f'<{port_split[1]}> does not contain "/"')
-        return port_split
-
-    @property
-    def cidr(self) -> [str]:
-        return self.cidr
+            raise ValueError(f'port number <{port_split[0]}> <{port_split[1]}> does not contain "/"')
+        self._port = port_split
+        self.init_setup()
 
     @cidr.setter
-    def cidr(self,cidr) -> [str]:
+    def cidr(self,cidr):
         cidr_split = re.split("/",cidr)
         if len(cidr_split) != 2:
-            raise ValueError(f'<{cidr}> does not contain "/"')
+            raise ValueError(f'cidr <{cidr}> does not contain "/" or is invalid')
+        cidr_split = [value.strip() for value in cidr_split]
+        print(f'CIDR Setter cidr_split: {cidr_split}')
         valid_subnet_address = '\d{,3}.\d{,3}.\d{,3}.\d{,3}'
         if not bool(re.match(valid_subnet_address,cidr_split[0])):
             raise ValueError(f'<{cidr_split[0]}> is not a valid subnet address')
-        cidr_netmask = int(cidr_split[1])
-        if cidr_netmask < 0 or cidr_netmask > 32:
+        if '/' in cidr_split[1]:
+            cidr_split[1] = cidr_split[1].replace("/","")
+        prefix = int(cidr_split[1])
+        print(f'CIDR Setter prefix: {prefix}')
+        if prefix < 0 or prefix > 32:
             raise ValueError(f"<{cidr_split[1]}> is not a valid subnet mask")
-        network,netmask = self.cidr_to_netmask(cidr)
-        self.cidr = [network,netmask]
+        network,netmask = self.cidr_to_netmask('/'.join(cidr_split))
+        self.netmask = netmask
+        print('<'+network+'>')
+        print('<'+str(prefix)+'>')
+        self._cidr = [network,str(prefix)]
+        print(f'CIDR Setter self._cidr: {self._cidr}')
+        if self._ip_address != None:
+            self.config_setup()
+
+    @ip_address.setter
+    def ip_address(self,ip_address):
+        # Remove spaces and split ip address by "."
+        split_ip_address = ip_address.replace(" ","").split(".")
+        valid_ip_address = '\d{,3}.\d{,3}.\d{,3}.\d{,3}'
+        # If the IP address does not match X.X.X.X, raise a ValueError.
+        if not bool(re.match(valid_ip_address, ip_address)):
+            raise ValueError(f'<{ip_address}> is not a valid IP address')
+        self._ip_address = split_ip_address
+        self.config_setup()
 
     @staticmethod
     # https://stackoverflow.com/questions/33750233/convert-cidr-to-subnet-mask-in-python
@@ -73,17 +123,11 @@ class Interface(object):
         netmask = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
         return network, netmask
 
-    def initial_setup(self):
+    def init_setup(self):
         # Enter the interface.
-        self.commamnds += ["int %s" % self.port]
-        # Make sure you can change the interface.
-        self.commands  += ["no switchport"]
+        self.init_commands[0] = "int %s" % self.port
 
-    def configure_setup(self):
-        ["ip address %s %s" % (ip address, subnet_mask)]
 
-    def exit_setup(self):
-        # Stop the port interface from going down
-        ["no shut"]
-        # Get out of the interface
-        ["exit"]
+    def config_setup(self):
+        self.config_commands = ["ip address %s %s" % (self.ip_address, self.netmask)]
+
