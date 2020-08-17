a3c0580b1f58f72225206f0bf05e86e17fb1cbe2
David Warshawsky <david.warshawsky@sjsu.edu>
2020-08-16 22:23:14 -0700

    I changed the router module so that way the Router class is iterable and keeps track of Routers that exist. I used a dictionary with the protocol and the activation status defaulted to False to keep track of which protocols were enabled. I split add_configuration method into activate_configuration() and add_configuration() methods. The activate_configuration() sets a certain protocol to True while add_configuration is used to add commands for the protocols when the user has finished playing with the settings.

--- router.py
+++ router.py
@@ -1,47 +1,76 @@
-import weakref
-from ipaddress import IPv4Network, IPv4Address
-from interfaces import Interface
 import os
-class Router:
-    hostname = None
-    commands = ["en","conf t","no ip domain-lookup"]
+from interface import Interface
+from protocols import RIP2
+
+
+class IterRouter(type):
+    def __iter__(cls):
+        return iter(cls._all_routers)
+
+class Router(metaclass=IterRouter):
+    _all_routers = []
+    available_protocols = ['RIP1','RIP2','OSPF1','OSPF2']
 
     def __init__(self,hostname):
         # If the hostname already exists, then raise a ValueError
-        if sum([hostname == router.hostname for router in self.getinstances()]) != 0:
+        double_list = [hostname == router.hostname for router in self._all_routers]
+        if sum(double_list) != 0:
             raise ValueError(f"Router hostname <{hostname}> already exists")
+        # Add new router to list of instances.
+        Router._all_routers.append(self)
+        # Set class instance attributes.
         self.hostname = hostname
-        self.commands += ["hostname %s" % self.hostname]
+        self.commands = ["en", "conf t", "no ip domain-lookup"]
+        self.interfaces: [Interface] = []
+        self.activated_protocols: {str: bool} = {'RIP1': False, 'RIP2': False, 'OSPF1': False, 'OSPF2': False}
 
     def add_interface(self,interface:Interface):
+        # If the interface already exists, raise a ValueError.
+        if len(self.interfaces) != 0:
+            if sum([str(interface) == str(current_interface) for current_interface in self.interfaces]) != 0:
+                raise ValueError('This interface already exists')
         self.interfaces.append(interface)
 
-    # Used to check that two routers with the same hostname do not exist.
-    @classmethod
-    def getinstances(cls):
-        dead = set()
-        for ref in cls._instances:
-            obj = ref()
-            if obj is not None:
-                yield obj
-            else:
-                dead.add(ref)
-        cls._instances -= dead
+    def activate_protocol(self,protocol):
+        "ONLY ADD PROTOCOLS AT THE END of the program!"
+        if protocol not in self.available_protocols:
+            raise ValueError(f"This is not a valid protocol: {protocol}")
+        else:
+            self.activated_protocols[protocol] = True
 
-    def configure_interfaces(self):
-        for interface,values in self.networks.items():
+    def add_protocol_config(self,protocol):
+        if protocol == 'RIP1':
+            pass
+        elif protocol == 'RIP2':
+            # Configure and add networks and then exit.
+            commands = ['ip routing','router rip','version 2','no auto-summary']
+            for interface in self.interfaces:
+                commands.append("network %s" % interface._cidr[0])
+            self.commands.extend(commands)
+            self.commands.append('exit')
+        elif protocol == 'OSPF1':
+            pass
+        elif protocol == 'OSPF2':
+            pass
+        else:
+            raise ValueError(f"<{protocol}> is Not a Valid Protocol")
 
-
-    def done(self):
-        # ["end", "wr mem"]
-
-        # Configure rip
+    def exit(self):
+        # Add the hostname to the commands
+        self.commands += ["hostname %s" % self.hostname]
+        # Add interface configurations to commands.
+        for interface in self.interfaces:
+            self.commands.extend(interface.commands)
+        # Add each protocol if activated.
+        for protocol,activated in self.protocols:
+            if activated:
+                self.add_protocol_config(protocol)
         self.commands += ["end","wr mem"]
         self.generate_output()
 
 
-    def generate_output(self):
-        filename = "%s.txt" %self.hostname
+    def generate_output(self,folder):
+        filename = os.path.join(folder,"%s.txt" %self.hostname)
         if (os.path.isfile(filename)):
             raise ValueError(f"There is already a file called {filename}")
         output_file = open(filename, "w")
@@ -49,9 +78,7 @@ class Router:
             output_file.write(command + "\n ! \n")
         output_file.close()
 
-
-
-
+    # Used to check that two routers with the same hostname do not exist.
 
 
 
