264890e96480762862fff3def94f441603990e7b
David Warshawsky <david.warshawsky@sjsu.edu>
2020-08-16 22:21:04 -0700

    I created a class for each protocol and added commands associated with the protocol. I made sure that entering and exiting was provided so that way transition would be made easier.

--- protocols.py
+++ protocols.py
@@ -1,13 +1,26 @@
+from interface import Interface
 #     def configure_rip(self):
 #         for addr in self.get_net_addrs():
-#             self.rip_commands.append("network %s" % addr)
+#             self.rip_commands.append("networks %s" % addr)
 #         self.rip_commands.append("exit")
 #         self.commands += self.rip_commands
 
-class RIP(object):
-    rip_commands = ["ip routing", "router rip", "version 2", "no auto-summary"]
+class RIP2(object):
+    """
+    This class is meant to be initialized at the end of the program. It will take all networks that are
+    desired and add rip commands to the Router.
+    """
+    commands = ["ip routing", "router rip", "version 2", "no auto-summary"]
+    networks   = []
+    def __init__(self,interfaces:[Interface]):
+        self.networks = [interface.cidr[0] for interface in interfaces]
+        network_commands = ["networks %s" % network for network in self.networks]
+        self.commands.extend(network_commands)
+        self.commands.append("exit")
+
+
 
 
 
 class OSPF(object):
-
+    pass
