8291d90288073e3d004201c1245a7ccc7e0da6dc
davidwarshawsky <davidawarshawsky@gmail.com>
2020-09-06 13:22:43 -0700

    Added notes that Professor Nguyen shared.

--- src/protocols.py
+++ src/protocols.py
@@ -18,6 +18,9 @@ class RIP2(object):
         self.commands.extend(network_commands)
         self.commands.append("exit")
 
+# Pay attention to management side. Many tools free to simulate.
+# NGSoft SMTP, Enterprise industry network management tool.
+# TODO: Add
 
 
 
